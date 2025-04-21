f40c5d59e500652ead49c1bb45d4165510a7bd0e
dhadirca <dhadirca@adobe.com>
2025-04-21 13:37:39 +0300

    support for rabbitmq

--- temporal_stuff/workflows.py
+++ temporal_stuff/workflows.py
@@ -1,4 +1,4 @@
-import asyncio 
+import asyncio
 from datetime import timedelta
 
 from temporalio import workflow
@@ -6,7 +6,8 @@ from temporalio.common import RetryPolicy
 
 with workflow.unsafe.imports_passed_through():
     from temporal_stuff.activities import Activities
-    from temporal_stuff.shared import Message, MessageRequest, MessageContext, WorkflowState, RabbitMqQueueParams, ASSISTANT_QUEUE
+    from temporal_stuff.shared import Message, MessageRequest, MessageContext, WorkflowState, RabbitMqQueueParams, \
+        ASSISTANT_QUEUE
 
 
 @workflow.defn
@@ -14,26 +15,87 @@ class AssistantWorkflow:
 
     def __init__(self) -> None:
         self.pending_message_queue: asyncio.Queue[str] = asyncio.Queue()
-        self._exit = False 
+        self._exit = False
         self.workflow_state = WorkflowState(
             intent_detection={},
-            disambiguate={},
+            disambiguate=False,
             oos_output="",
-            system_output="",
+            system_output={},
             current_message=None,
             chat_history=[],
-            workflow_id=None,
+            workflow_id="",
             context={}
         )
-    
+
+        self.retry_policy = RetryPolicy(
+            maximum_attempts=3,
+            maximum_interval=timedelta(seconds=2)
+        )
+
+    async def finalize_workflow(self, response):
+        if response['intent'] == "suggestion":
+            suggestion = await workflow.execute_activity_method(
+                Activities.get_suggestion,
+                args=[response, self.workflow_state.current_message.message],
+                start_to_close_timeout=timedelta(seconds=10),
+                retry_policy=self.retry_policy
+            )
+
+            suggestion_prompt = suggestion.suggestion_prompt
+            suggestion_type = suggestion.suggestion_type
+
+            if "[ambiguity detected]" in suggestion_prompt:
+                self.workflow_state.disambiguate = True
+            else:
+                self.workflow_state.disambiguate = False
+
+            self.workflow_state.system_output = suggestion
+            print(f"suggestion: {suggestion}")
+
+            message_struct = Message(
+                message=self.workflow_state.current_message.message,
+                response=response["message"]
+            )
+
+            self.workflow_state.chat_history.append(message_struct)
+
+        elif response["intent"] == "explanation":
+            explanation = await workflow.execute_activity_method(
+                Activities.get_explanation,
+                args=[response, self.workflow_state.current_message.message],
+                start_to_close_timeout=timedelta(seconds=10),
+                retry_policy=self.retry_policy
+            )
+
+            self.workflow_state.system_output = explanation
+            print(f"explanation: {explanation}")
+
+            message_struct = Message(
+                message=self.workflow_state.current_message.message,
+                response=response["message"]
+            )
+
+            self.workflow_state.chat_history.append(message_struct)
+
+        else:
+            response = "I dont understand what are you saying"
+            message = Message(
+                message=self.workflow_state.current_message.message,
+                response=response
+            )
+
+            self.workflow_state.chat_history.append(message)
+
+        print(f"Workflow state: {self.workflow_state}")
+
+
     @workflow.query(name="get_status")
     def get_status(self) -> WorkflowState:
         return self.workflow_state
-    
+
     @workflow.signal
-    async def submit_user_input(self,m:str) -> None:
+    async def submit_user_input(self, m: str) -> None:
         await self.pending_message_queue.put(m)
-    
 
     @workflow.signal
     def exit(self) -> None:
@@ -42,24 +104,18 @@ class AssistantWorkflow:
     @workflow.run
     async def run(self, request: MessageRequest) -> WorkflowState:
         print("Start Workflow...")
-        self.workflow_state.current_message = Message(message=request.message,response="")
+        self.workflow_state.current_message = Message(message=request.message, response="")
         self.workflow_state.workflow_id = request.workflow_id
         self.workflow_state.context = request.context
 
-        retry_policy = RetryPolicy(
-            maximum_attempts=3,
-            maximum_interval=timedelta(seconds=2)
-        )
+        user_input: str = ""  # used to receive signals on different stages of the workflow state
 
-        user_input:str = None # used to receive signals on different stages of the workflow state
-
-        
         # ACTIVITY INTENT
         message_info = await workflow.execute_activity_method(
             Activities.detect_intent,
             self.workflow_state.current_message,
             start_to_close_timeout=timedelta(seconds=10),
-            retry_policy=retry_policy
+            retry_policy=self.retry_policy
         )
 
         self.workflow_state.intent_detection = message_info
@@ -72,19 +128,18 @@ class AssistantWorkflow:
             self.workflow_state.current_message.response = "I can't help you"
             print("Terminating...")
             return self.workflow_state
-        
 
         message_context = MessageContext(
             message_info=message_info,
             context=self.workflow_state.context
         )
 
-
+        # ACTIVITY ANALYZE INFO
         response = await workflow.execute_activity_method(
             Activities.analyze_info,
             message_context,
             start_to_close_timeout=timedelta(seconds=10),
-            retry_policy=retry_policy
+            retry_policy=self.retry_policy
         )
 
         print(f"response: {response}")
@@ -99,7 +154,7 @@ class AssistantWorkflow:
             )
 
             queue_params = RabbitMqQueueParams(
-                queue_name=self.workflow_state.workflow_id,
+                queue_name="ASSISTANT_QUEUE",
                 exchange="",
                 message=response["message"]
             )
@@ -108,7 +163,7 @@ class AssistantWorkflow:
                 Activities.publish_message,
                 queue_params,
                 start_to_close_timeout=timedelta(seconds=5),
-                retry_policy=retry_policy
+                retry_policy=self.retry_policy
             )
 
             self.workflow_state.chat_history.append(history_query)
@@ -120,13 +175,13 @@ class AssistantWorkflow:
                 message = self.pending_message_queue.get_nowait()
                 print(f"User input for clarification: {message}")
 
-                self.workflow_state.current_message.message=message
+                self.workflow_state.current_message.message = message
 
                 clarifying_message_info = await workflow.execute_activity_method(
                     Activities.detect_intent,
                     self.workflow_state.current_message,
                     start_to_close_timeout=timedelta(seconds=10),
-                    retry_policy=retry_policy
+                    retry_policy=self.retry_policy
                 )
 
                 self.workflow_state.intent_detection = clarifying_message_info
@@ -142,10 +197,10 @@ class AssistantWorkflow:
                     Activities.analyze_info,
                     clarifying_message_context,
                     start_to_close_timeout=timedelta(seconds=10),
-                    retry_policy=retry_policy
+                    retry_policy=self.retry_policy
                 )
 
-                self.workflow_state.current_message.response=clarifying_response["message"]
+                self.workflow_state.current_message.response = clarifying_response["message"]
                 self.workflow_state.disambiguate = clarifying_response["disambiguate"]
                 print(f"clarifying response: {clarifying_response}")
 
@@ -153,52 +208,41 @@ class AssistantWorkflow:
                 if self._exit:
                     break
 
-        if response['intent'] == "suggestion":
-            suggestion = await workflow.execute_activity_method(
-                Activities.get_suggestion,
-                args=[response,self.workflow_state.current_message.message],
-                start_to_close_timeout=timedelta(seconds=10),
-                retry_policy=retry_policy
-            )
+        await self.finalize_workflow(response)
 
-            self.workflow_state.system_output=suggestion
-            print(f"suggestion: {suggestion}")
+        while self.workflow_state.disambiguate:
 
-            request = Message(
+            history_query = Message(
                 message=self.workflow_state.current_message.message,
-                response=response["message"]
+                response=self.workflow_state.current_message.response
             )
 
-            self.workflow_state.chat_history.append(request)
-
-        elif response["intent"] == "explanation":
-            explanation = await workflow.execute_activity_method(
-                Activities.get_explanation,
-                args=[response,self.workflow_state.current_message.message],
-                start_to_close_timeout=timedelta(seconds=10),
-                retry_policy=retry_policy
+            queue_params = RabbitMqQueueParams(
+                queue_name="ASSISTANT_QUEUE",
+                exchange="",
+                message=response["message"]
             )
 
-            self.workflow_state.system_output=explanation
-            print(f"explanation: {explanation}")
-
-            request = Message(
-                message=self.workflow_state.current_message.message,
-                response=response["message"]
+            await workflow.execute_activity_method(
+                Activities.publish_message,
+                queue_params,
+                start_to_close_timeout=timedelta(seconds=5),
+                retry_policy=self.retry_policy
             )
 
-            self.workflow_state.chat_history.append(request)
-
-        else:
-            response = "I dont understand what are you saying"
-            message = Message(
-                message=self.workflow_state.current_message.message,
-                response=response
+            self.workflow_state.chat_history.append(history_query)
+            await workflow.wait_condition(
+                lambda: not self.pending_message_queue.empty() or self._exit
             )
 
-            self.workflow_state.chat_history.append(message)
-        
-        print(f"FINAL workflow state: {self.workflow_state}")
-        print("Closing...")
+            while not self.pending_message_queue.empty():
+                message = self.pending_message_queue.get_nowait()
+                print(f"User input for clarification: {message}")
+
+                self.workflow_state.current_message.message = message
+                self.workflow_state.disambiguate = False
+                break
+
+
+        print("CLOSING...")
         return self.workflow_state
-        
